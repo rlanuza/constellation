850abdd0cb65837ee0160dd2284c10a679eb12a0
Rolf <rolf2000@gmail.com>
2018-05-17 23:31:24 +0200

    Compare four methods

---
+++ run4.cmd
@@ -0,0 +1,8 @@
+java -jar dist\constellation.jar
+
+start java -jar dist\constellation.jar --parameters:data\constellation0.txt
+start java -jar dist\constellation.jar --parameters:data\constellation1.txt
+start java -jar dist\constellation.jar --parameters:data\constellation2.txt
+start java -jar dist\constellation.jar --parameters:data\constellation3.txt
+
+timeout 60
