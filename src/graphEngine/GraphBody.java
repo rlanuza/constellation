deaee41dbe4d3c17bff4e9272826e8098d28c6fe
Rolf <rolf2000@gmail.com>
2019-03-13 23:24:30 +0100

    Document the code interface

--- src/graphEngine/GraphBody.java
+++ src/graphEngine/GraphBody.java
@@ -2,16 +2,49 @@ package graphEngine;
 
 import java.awt.Color;
 
-/*@Todo: comentar fichero $$$$$$$$$ESTOY COMENTANDO AQUI $$$$$$$$$*/
+/**
+ * Represents the essential information that characterize a graphical body
+ *
+ * @author Roberto Lanuza rolf2000@gmail.com
+ * @version 1.0
+ */
 public final class GraphBody {
 
+    /**
+     * Index of the body used to let us indexed access.
+     */
     int index;
+    /**
+     * Name of the body e.g: "Earth".
+     */
     String name;
+    /**
+     * Average radius of the body. Units m.
+     */
     double radius;
+    /**
+     * Scaled radius of the body in integer notation.
+     */
     int radius_i;
+    /**
+     * Color asigned to the body.
+     */
     Color color;
+    /**
+     * Graphical body orbit.
+     */
     GraphOrbit orbit;
 
+    /**
+     * Create a new graphical body.
+     *
+     * @param Index of the body used to let us indexed access.
+     * @param name Name of the body.
+     * @param radius Average radius of the body. Unit m.
+     * @param Color asigned to the body.
+     * @param orbit body orbit.
+     * @param scale scale used to convert to graphical.
+     */
     public GraphBody(int index, String name, double radius, Color color, GraphOrbit orbit, double scale) {
         this.index = index;
         this.name = name;
@@ -22,6 +55,9 @@ public final class GraphBody {
         recalculateGrRadius(scale);
     }
 
+    /**
+     * Recalculate the scaled radius of the body with the scale.
+     */
     public void recalculateGrRadius(double scale) {
         radius_i = (int) (radius * scale);
     }
